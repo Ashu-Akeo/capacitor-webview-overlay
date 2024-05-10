74464a5f8677141839e4c7beafa5a9f36e4968de
Ashutosh <ashutosh@akeo.no>
2024-05-10 14:52:40 +0530

    included dist folder to be able directly install as a package

---
+++ dist/esm/plugin.d.ts
@@ -0,0 +1,52 @@
+import type { PluginListenerHandle } from '@capacitor/core';
+import { ResizeObserver } from '@juggle/resize-observer';
+import { ScriptInjectionTime } from './definitions';
+export interface WebviewOverlayOpenOptions {
+    /**
+     * The URL to open the webview to
+     */
+    url: string;
+    script?: {
+        javascript: string;
+        injectionTime?: ScriptInjectionTime;
+    };
+    /**
+     * The element to open the webview in place of. The webview will open with the same dimensions and fixed position on screen.
+     * When toggled off, the element will have a background image with the webview snapshot.
+     */
+    element: HTMLElement;
+    /**
+     * Allow use append the string to the end of the user agent.
+     */
+    userAgent?: string;
+}
+declare class WebviewOverlayClass {
+    element: HTMLElement;
+    updateSnapshotEvent: PluginListenerHandle;
+    pageLoadedEvent: PluginListenerHandle;
+    progressEvent: PluginListenerHandle;
+    navigationHandlerEvent: PluginListenerHandle;
+    resizeObserver: ResizeObserver;
+    open(options: WebviewOverlayOpenOptions): Promise<void>;
+    close(): Promise<void>;
+    toggleSnapshot(snapshotVisible: boolean): Promise<void>;
+    b64toByteArr: (b64Data: any, sliceSize?: number) => any[];
+    evaluateJavaScript(javascript: string): Promise<string>;
+    onPageLoaded(listenerFunc: () => void): void;
+    onProgress(listenerFunc: (progress: {
+        value: number;
+    }) => void): void;
+    handleNavigation(listenerFunc: (event: {
+        url: string;
+        newWindow: boolean;
+        sameHost: boolean;
+        complete: (allow: boolean) => void;
+    }) => void): void;
+    toggleFullscreen(): void;
+    goBack(): void;
+    goForward(): void;
+    reload(): void;
+    loadUrl(url: string): Promise<void>;
+}
+export declare const WebviewOverlay: WebviewOverlayClass;
+export {};
---
+++ dist/plugin.cjs.js
@@ -0,0 +1,171 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var core = require('@capacitor/core');
+var resizeObserver = require('@juggle/resize-observer');
+
+exports.ScriptInjectionTime = void 0;
+(function (ScriptInjectionTime) {
+    ScriptInjectionTime[ScriptInjectionTime["atDocumentStart"] = 0] = "atDocumentStart";
+    ScriptInjectionTime[ScriptInjectionTime["atDocumentEnd"] = 1] = "atDocumentEnd";
+})(exports.ScriptInjectionTime || (exports.ScriptInjectionTime = {}));
+
+const WebviewOverlayPlugin = core.registerPlugin('WebviewOverlayPlugin');
+class WebviewOverlayClass {
+    constructor() {
+        this.b64toByteArr = (b64Data, sliceSize = 512) => {
+            const byteCharacters = atob(b64Data);
+            const byteArrays = [];
+            for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                const slice = byteCharacters.slice(offset, offset + sliceSize);
+                const byteNumbers = new Array(slice.length);
+                for (let i = 0; i < slice.length; i++) {
+                    byteNumbers[i] = slice.charCodeAt(i);
+                }
+                const byteArray = new Uint8Array(byteNumbers);
+                byteArrays.push(byteArray);
+            }
+            // const blob = new Blob(byteArrays, {type: contentType});
+            return byteArrays;
+        };
+    }
+    open(options) {
+        this.element = options.element;
+        if (this.element && this.element.style) {
+            this.element.style.backgroundSize = 'cover';
+            this.element.style.backgroundRepeat = 'no-repeat';
+            this.element.style.backgroundPosition = 'center';
+        }
+        const boundingBox = this.element.getBoundingClientRect();
+        this.updateSnapshotEvent = WebviewOverlayPlugin.addListener('updateSnapshot', () => {
+            setTimeout(() => {
+                this.toggleSnapshot(true);
+            }, 100);
+        });
+        this.resizeObserver = new resizeObserver.ResizeObserver((entries) => {
+            for (const _entry of entries) {
+                const boundingBox = options.element.getBoundingClientRect();
+                WebviewOverlayPlugin.updateDimensions({
+                    width: Math.round(boundingBox.width),
+                    height: Math.round(boundingBox.height),
+                    x: Math.round(boundingBox.x),
+                    y: Math.round(boundingBox.y)
+                });
+            }
+        });
+        this.resizeObserver.observe(this.element);
+        return WebviewOverlayPlugin.open({
+            url: options.url,
+            javascript: options.script ? options.script.javascript : '',
+            userAgent: options.userAgent ? options.userAgent : '',
+            injectionTime: options.script ? (options.script.injectionTime || exports.ScriptInjectionTime.atDocumentStart) : exports.ScriptInjectionTime.atDocumentStart,
+            width: Math.round(boundingBox.width),
+            height: Math.round(boundingBox.height),
+            x: Math.round(boundingBox.x),
+            y: Math.round(boundingBox.y)
+        });
+    }
+    close() {
+        this.element = undefined;
+        this.resizeObserver.disconnect();
+        if (this.updateSnapshotEvent) {
+            this.updateSnapshotEvent.remove();
+        }
+        if (this.pageLoadedEvent) {
+            this.pageLoadedEvent.remove();
+        }
+        if (this.progressEvent) {
+            this.progressEvent.remove();
+        }
+        if (this.navigationHandlerEvent) {
+            this.navigationHandlerEvent.remove();
+        }
+        return WebviewOverlayPlugin.close();
+    }
+    async toggleSnapshot(snapshotVisible) {
+        return new Promise(async (resolve) => {
+            if (snapshotVisible) {
+                const snapshot = (await WebviewOverlayPlugin.getSnapshot()).src;
+                if (snapshot) {
+                    // const buffer = await (await fetch('data:image/jpeg;base64,' + snapshot)).arrayBuffer();
+                    // reference: https://stackoverflow.com/a/16245768
+                    /* const byteCharacters = atob(snapshot);
+                    const byteNumbers = new Array(byteCharacters.length);
+                    for (let i = 0; i < byteCharacters.length; i++) {
+                        byteNumbers[i] = byteCharacters.charCodeAt(i);
+                    }
+                    const buffer = new Uint8Array(byteNumbers); */
+                    const buffer = this.b64toByteArr(snapshot);
+                    // const blob = new Blob([buffer], { type: 'image/jpeg' });
+                    const blob = new Blob(buffer, { type: 'image/jpeg' });
+                    const blobUrl = URL.createObjectURL(blob);
+                    const img = new Image();
+                    console.warn('img.onload', blobUrl);
+                    img.onload = async () => {
+                        if (this.element && this.element.style) {
+                            this.element.style.backgroundImage = `url(${blobUrl})`;
+                        }
+                        setTimeout(async () => {
+                            await WebviewOverlayPlugin.hide();
+                            resolve();
+                        }, 25);
+                    };
+                    img.src = blobUrl;
+                }
+                else {
+                    if (this.element && this.element.style) {
+                        this.element.style.backgroundImage = `none`;
+                    }
+                    await WebviewOverlayPlugin.hide();
+                    resolve();
+                }
+            }
+            else {
+                if (this.element && this.element.style) {
+                    this.element.style.backgroundImage = `none`;
+                }
+                await WebviewOverlayPlugin.show();
+                resolve();
+            }
+        });
+    }
+    async evaluateJavaScript(javascript) {
+        return (await WebviewOverlayPlugin.evaluateJavaScript({
+            javascript
+        })).result;
+    }
+    onPageLoaded(listenerFunc) {
+        this.pageLoadedEvent = WebviewOverlayPlugin.addListener('pageLoaded', listenerFunc);
+    }
+    onProgress(listenerFunc) {
+        this.progressEvent = WebviewOverlayPlugin.addListener('progress', listenerFunc);
+    }
+    handleNavigation(listenerFunc) {
+        this.navigationHandlerEvent = WebviewOverlayPlugin.addListener('navigationHandler', (event) => {
+            const complete = (allow) => {
+                WebviewOverlayPlugin.handleNavigationEvent({ allow });
+            };
+            listenerFunc(Object.assign(Object.assign({}, event), { complete }));
+        });
+    }
+    toggleFullscreen() {
+        WebviewOverlayPlugin.toggleFullscreen();
+    }
+    goBack() {
+        WebviewOverlayPlugin.goBack();
+    }
+    goForward() {
+        WebviewOverlayPlugin.goForward();
+    }
+    reload() {
+        WebviewOverlayPlugin.reload();
+    }
+    loadUrl(url) {
+        return WebviewOverlayPlugin.loadUrl({ url });
+    }
+}
+const WebviewOverlay = new WebviewOverlayClass();
+
+exports.WebviewOverlay = WebviewOverlay;
+//# sourceMappingURL=plugin.cjs.js.map
